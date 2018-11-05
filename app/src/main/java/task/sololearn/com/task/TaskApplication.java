89ccb66585aeb61a574054c682268b922107c945
hrataci <khachatr.karen@gmail.com>
2018-11-06 01:10:06 +0400

    timer service

--- app/src/main/java/task/sololearn/com/task/TaskApplication.java
+++ app/src/main/java/task/sololearn/com/task/TaskApplication.java
@@ -4,14 +4,21 @@ import android.app.Application;
 import android.arch.lifecycle.LifecycleObserver;
 import android.arch.lifecycle.ProcessLifecycleOwner;
 
+import com.firebase.jobdispatcher.Constraint;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
 
+import task.sololearn.com.task.services.TimerJobService;
 import task.sololearn.com.task.utils.AppLifecycleObserver;
+import task.sololearn.com.task.utils.Constants;
 import task.sololearn.com.task.utils.PrefManager;
 
 public class TaskApplication extends Application implements LifecycleObserver {
-    static FirebaseJobDispatcher dispatcher;
+    private static FirebaseJobDispatcher dispatcher;
 
     @Override
     public void onCreate() {
@@ -30,6 +37,19 @@ public class TaskApplication extends Application implements LifecycleObserver {
                 PrefManager.getInstance().setIsBackgrounded(true);
             }
         }));
+        startTimer();
+    }
+
+    public static void startTimer() {
+        Job myJob = dispatcher.newJobBuilder()
+                .setService(TimerJobService.class)
+                .setTrigger(Trigger.executionWindow(Constants.Timer.SECONDS, Constants.Timer.SECONDS+1))
+                .setTag("db-update-job")
+                .setLifetime(Lifetime.FOREVER)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .build();
+        dispatcher.mustSchedule(myJob);
+
     }
 
 }
--- app/src/main/java/task/sololearn/com/task/services/TimerJobService.java
+++ app/src/main/java/task/sololearn/com/task/services/TimerJobService.java
@@ -1,12 +1,18 @@
 package task.sololearn.com.task.services;
 
+import android.util.Log;
+
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+
+import task.sololearn.com.task.TaskApplication;
 
 public class TimerJobService extends JobService {
     @Override
     public boolean onStartJob(JobParameters job) {
+        Log.d("TimerJobService","job dispatching");
         // Do some work here
+        TaskApplication.startTimer();
         return false; // Answers the question: "Is there still work going on?"
     }
 
